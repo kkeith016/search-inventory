a94cfbf1c2ada2c1809550e18ae21ef967b15d4e
Kylee <kkeith.biz@gmail.com>
2025-10-08 10:19:13 -0400

    products getters and setters

--- src/main/java/com/pluralsight/main.java
+++ src/main/java/com/pluralsight/main.java
@@ -1,7 +1,11 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-
+        ArrayList<Product> inventory = getInventory();
+        Scanner scan = new Scanner(System.in);
     }
 }
